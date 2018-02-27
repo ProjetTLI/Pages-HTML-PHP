5e9e120ce8d97084bf758feff7878bb1e0ced24f
Capgir <36543767+Capgir@users.noreply.github.com>
2018-02-27 14:53:40 +0100

    Update inscription1.php

--- inscription1.php
+++ inscription1.php
@@ -1,18 +1,33 @@
 <?php
-// on teste si le visiteur a soumis le formulaire
-if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
-	// on teste l'existence de nos variables. On teste également si elles ne sont pas vides
-	if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass'])) && (isset($_POST['pass_confirm']) && !empty($_POST['pass_confirm']))) {
-	// on teste les deux mots de passe
-	if ($_POST['pass'] != $_POST['pass_confirm']) {
-		$erreur = 'Les 2 mots de passe sont différents.';
-	}
-	else {
-		$bdd = new PDO('mysql:host=localhost;dbname=user;charset=utf8', 'root', '');
-	}
-	catch (Exception $e)
-	{
-        	die('Erreur : ' . $e->getMessage());
-	}
-		coucou
+if(isset($_POST['submit']))
+{
+    $login = $_POST['login'];
+    $pass = $_POST['pass'];
+    $pass_confirm = $_POST['pass_confirm'];
+    $email = $_POST['email'];
+ 
+    if($login && $pass && $pass_confirm && $email)
+    {
+        if($passe == $passe2)
+        {
+            try {
+                try {
+                $dbh = new PDO('mysql:host=localhost;dbname=user', 'root', '');
+                array(PDO::ATTR_PERSISTENT => true);
+                session_start();
+    		if (!isset($_SESSION['login'])) {
+    			header ('Location: mapage.tpl');
+    		exit();
+                }
+                catch (Exception $e)
+                {
+                die("Impossible de se connecter: " . $e->getMessage());
+                }
+ 
+        }
+        else print 'Les mots de passe ne sont pas identiques.';
+    }
+    else print 'Merci de remplir tous les champs';
+}
+ 
 ?>
